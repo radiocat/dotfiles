d47239e23d54b1be9b3ecb8fb5f03ec54df60ca1
radiocat <radiocat.app@gmail.com>
2016-05-02 00:55:10 +0900

    update for win.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 mklink %HOMEPATH%"\_vimrc" %HOMEPATH%"\dotfiles\vim\vimrc"
 mklink %HOMEPATH%"\_gvimrc" %HOMEPATH%"\dotfiles\vim\gvimrc"
+mklink /d %HOMEPATH%"\vimfiles" %HOMEPATH%"\.vim"
 
 exit 0
 
