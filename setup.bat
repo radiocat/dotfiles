795ed8a91a796aaed502baf9b5ebb66e3362d277
radiocat <radiocat.app@gmail.com>
2017-07-17 14:10:26 +0900

    setup.batの更新
    gitconfigをホームディレクトリにコピー

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 mklink %HOMEPATH%"\_vimrc" %HOMEPATH%"\dotfiles\vim\vimrc"
 mklink %HOMEPATH%"\_gvimrc" %HOMEPATH%"\dotfiles\vim\gvimrc"
 mklink /d %HOMEPATH%"\vimfiles" %HOMEPATH%"\.vim"
+mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 
 exit 0
 
